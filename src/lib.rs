93279d0eb0eec897632b1a9fe5704d5ad7aa7a14
Nerijus Arlauskas <nercury@gmail.com>
2015-08-24 23:29:50 +0300

    Travis build.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+/*!
+<a href="https://github.com/Nercury/bytecache-rs">
+    <img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png" alt="Fork me on GitHub">
+</a>
+<style>.sidebar { margin-top: 53px }</style>
+*/
+
 pub mod path;
 pub mod mem;
 pub mod file;
