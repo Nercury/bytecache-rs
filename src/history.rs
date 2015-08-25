efa10d94027172049a59212a1a4f9ae943a6ed5f
Nerijus Arlauskas <nercury@gmail.com>
2015-08-25 23:21:10 +0300

    History tests.

--- src/history.rs
+++ src/history.rs
@@ -50,8 +50,12 @@ impl<K> Bucket<K>
             K: Borrow<Q>,
             Q: Hash + Eq + RequiredBytes
     {
-        self.usage -= value.required_bytes();
-        self.items.remove(value)
+        if self.items.remove(value) {
+            self.usage -= value.required_bytes();
+            true
+        } else {
+            false
+        }
     }
 
     pub fn iter(&self) -> hash_set::Iter<K> {
@@ -192,7 +196,7 @@ impl<K> History<K>
     }
 
     /// Refresh the key to delay its removal or to insert it to history.
-    pub fn refresh(&mut self, key: K)
+    pub fn hit(&mut self, key: K)
         where
             K: Eq + Hash + RequiredBytes
     {
@@ -205,7 +209,7 @@ impl<K> History<K>
         self.next_bucket.insert(key);
 
         if self.next_bucket.usage() >= self.max_bucket_usage {
-            self.push_bucket();
+            self.burry_bucket();
         }
     }
 
@@ -267,7 +271,7 @@ impl<K> History<K>
         }
     }
 
-    fn push_bucket(&mut self)
+    fn burry_bucket(&mut self)
         where K: RequiredBytes
     {
         let new_bucket = if self.buckets.len() as u64 >= self.bucket_count {
@@ -288,10 +292,144 @@ impl<K> History<K>
 
 #[cfg(test)]
 mod test {
+    use std::collections::BTreeSet;
+    use std::hash::Hash;
+    use RequiredBytes;
     use super::*;
 
-    #[test]
-    fn store_and_get() {
+    #[derive(Hash, Eq, PartialEq, Ord, PartialOrd, Debug, Copy, Clone)]
+    struct Val {
+        num: i32,
+        usage: u64,
+    }
 
+    impl RequiredBytes for Val {
+        fn required_bytes(&self) -> u64 {
+            self.usage
+        }
+    }
+
+    #[test]
+    fn spills_oldest() {
+        let mut h = History::new(2, 2);
+        h.hit(Val { num: 1, usage: 2 });
+        h.hit(Val { num: 2, usage: 2 });
+        h.hit(Val { num: 3, usage: 2 });
+
+        assert_eq!(vec![2, 2, 2, 0], h.detailed_usage());
+        assert_eq!(6, h.usage());
+
+        assert_eq!(vec![Val { num: 1, usage: 2 }], spill_and_get_sorted(&mut h));
+
+        assert_eq!(vec![0, 2, 2, 0], h.detailed_usage());
+        assert_eq!(4, h.usage());
+    }
+
+    #[test]
+    fn supports_oversized() {
+        let mut h = History::new(2, 2);
+        h.hit(Val { num: 1, usage: 4 });
+        h.hit(Val { num: 2, usage: 5 });
+        h.hit(Val { num: 3, usage: 6 });
+
+        assert_eq!(vec![4, 5, 6, 0], h.detailed_usage());
+        assert_eq!(15, h.usage());
+
+        assert_eq!(vec![Val { num: 1, usage: 4 }], spill_and_get_sorted(&mut h));
+
+        assert_eq!(vec![0, 5, 6, 0], h.detailed_usage());
+        assert_eq!(11, h.usage());
+    }
+
+    #[test]
+    fn supports_small() {
+        let mut h = History::new(2, 2);
+        h.hit(Val { num: 1, usage: 1 });
+        h.hit(Val { num: 2, usage: 1 });
+        h.hit(Val { num: 3, usage: 1 });
+
+        assert_eq!(vec![0, 2, 1], h.detailed_usage());
+        assert_eq!(3, h.usage());
+
+        h.hit(Val { num: 4, usage: 1 });
+
+        assert_eq!(vec![0, 2, 2, 0], h.detailed_usage());
+        assert_eq!(4, h.usage());
+
+        h.hit(Val { num: 5, usage: 1 });
+        h.hit(Val { num: 6, usage: 1 });
+        h.hit(Val { num: 7, usage: 1 });
+
+        assert_eq!(vec![2, 2, 2, 1], h.detailed_usage());
+        assert_eq!(7, h.usage());
+
+        assert_eq!(vec![
+            Val { num: 1, usage: 1 },
+            Val { num: 2, usage: 1 }
+        ], spill_and_get_sorted(&mut h));
+
+        assert_eq!(vec![0, 2, 2, 1], h.detailed_usage());
+        assert_eq!(5, h.usage());
+    }
+
+    #[test]
+    fn digs_out_when_used_again() {
+        let mut h = History::new(2, 1);
+        h.hit(Val { num: 1, usage: 1 });
+        h.hit(Val { num: 2, usage: 1 });
+
+        h.hit(Val { num: 1, usage: 1 });
+
+        assert_eq!(vec![0, 1, 1], h.detailed_usage());
+        assert_eq!(2, h.usage());
+    }
+
+    #[test]
+    fn removes_recent() {
+        let mut h = History::new(2, 1);
+        h.hit(Val { num: 1, usage: 1 });
+        h.hit(Val { num: 2, usage: 1 });
+        h.hit(Val { num: 3, usage: 1 });
+
+        h.remove(&Val { num: 3, usage: 1 });
+
+        assert_eq!(vec![0, 2, 0], h.detailed_usage());
+        assert_eq!(2, h.usage());
+    }
+
+    #[test]
+    fn removes_burried() {
+        let mut h = History::new(2, 1);
+        h.hit(Val { num: 1, usage: 1 });
+        h.hit(Val { num: 2, usage: 1 });
+        h.hit(Val { num: 3, usage: 1 });
+
+        h.remove(&Val { num: 1, usage: 1 });
+        h.remove(&Val { num: 2, usage: 1 });
+
+        assert_eq!(vec![0, 0, 1], h.detailed_usage());
+        assert_eq!(1, h.usage());
+    }
+
+    #[test]
+    fn does_not_remove_old() {
+        let mut h = History::new(2, 1);
+        h.hit(Val { num: 1, usage: 1 });
+        h.hit(Val { num: 2, usage: 1 });
+        h.hit(Val { num: 3, usage: 1 });
+        h.hit(Val { num: 4, usage: 1 });
+
+        h.remove(&Val { num: 1, usage: 1 });
+        h.remove(&Val { num: 2, usage: 1 });
+
+        assert_eq!(vec![2, 2, 0], h.detailed_usage());
+        assert_eq!(4, h.usage());
+    }
+
+    fn spill_and_get_sorted<V>(history: &mut History<V>) -> Vec<V> where V: Eq + Hash + Copy + Ord {
+        let mut res = BTreeSet::new();
+        history.spill(&mut res);
+
+        res.iter().cloned().collect()
     }
 }
